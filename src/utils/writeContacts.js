84e20a6ab0b2754b40c341bf40127383d775ed55
Eugene-Mischenko <129785816+Eugene-Mischenko@users.noreply.github.com>
2025-06-12 14:23:23 +0300

    v1.0

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,12 @@
+import { promises as fs } from 'fs';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    const data = JSON.stringify(updatedContacts, null, 2);
+    await fs.writeFile(PATH_DB, data, 'utf8');
+    console.log('Contacts saved successfully');
+  } catch (error) {
+    console.error('Error saving contacts:', error);
+  }
+};
